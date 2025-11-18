24a31c5ef8d3cb411aaa5f863f0b4901ca4ad77d
manumoreira <manuelmoreira.ar@gmail.com>
2025-11-18 00:17:55 -0300

    bolu-fix

--- multi_execute.bash
+++ multi_execute.bash
@@ -1,6 +1,6 @@
 # Este loop permite programar la ejecución de muchos notebooks desde la terminal ssh
 # Solo tenemos que estar seguros de estar parados en el directorio correspondiente
-for nb in zlgbm.base.v1.ipynb zlgbm-base.ipynb; do
+for nb in zlgbm.base.v2.ipynb zlgbm.base.v1.ipynb; do
     echo "🚀 Executing: $nb"
     uv run jupyter nbconvert --execute --inplace "$nb"
     echo "✅ Finished: $nb"
