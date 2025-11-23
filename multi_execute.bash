a41dbd7f829c619e72804d8cbfa7a9ec6cfa0e06
manumoreira <manuelmoreira.ar@gmail.com>
2025-11-23 20:29:15 -0300

    update

--- multi_execute.bash
+++ multi_execute.bash
@@ -1,6 +1,6 @@
 # Este loop permite programar la ejecución de muchos notebooks desde la terminal ssh
 # Solo tenemos que estar seguros de estar parados en el directorio correspondiente
-for nb in zlgbm.base.v1.ipynb zlgbm.base.v2.ipynb zlgbm.base.v3.ipynb zlgbm.base.v4.ipynb zlgbm.base.v5.ipynb zlgbm.base.v6.ipynb zlgbm.base.v7.ipynb zlgbm.base.v8.ipynb; do
+for nb in zlgbm.IPC.v1.ipynb zlgbm.IPC.v2.ipynb zlgbm.IPC.v3.ipynb zlgbm.IPC.v4.ipynb zlgbm.IPC.v5.ipynb zlgbm.IPC.v6.ipynb zlgbm.Rank.v1.ipynb zlgbm.Rank.v2.ipynb zlgbm.Rank.v3.ipynb zlgbm.Rank.v4.ipynb zlgbm.Rank.v5.ipynb zlgbm.Rank.v6.ipynb; do
     echo "🚀 Executing: $nb"
     uv run jupyter nbconvert --execute --inplace "$nb"
     echo "✅ Finished: $nb"
